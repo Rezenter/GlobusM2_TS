0a29e05eacaeb9cb5186bcac31570126af9b0a31
rezenter <nisovru@gmail.com>
2021-02-27 15:41:04 +0300

    ???

--- python/utils/viewer/directReadFromFile/viewer.py
+++ python/utils/viewer/directReadFromFile/viewer.py
@@ -1,20 +1,47 @@
 import ijson
-import
 
-shotn = 383
-poly = '5'
+shotn = 39629
 adc_ind = 1
-adc_ch = 11
-threshold = 1200/2
+adc_chs = [
+    {
+        'gr': 0,
+        'ch': 1
+    },    {
+        'gr': 1,
+        'ch': 0
+    },    {
+        'gr': 1,
+        'ch': 1
+    },    {
+        'gr': 2,
+        'ch': 0
+    },    {
+        'gr': 2,
+        'ch': 1
+    },
+]
+event_ind = 60
 
 print('Loading raw...')
-with open('d:/data/db/debug/raw/%05d/%d.json' % (shotn, adc_ind), 'rb') as board_file:
-    raw = [[] for ch in range(5)]
+with open('d:/data/db/plasma/raw/%05d/%d.json' % (shotn, adc_ind), 'rb') as board_file:
+    raw = [[] for ch in range(len(adc_chs))]
     skip = True
+    curr = 0
     for event in ijson.items(board_file, 'item', use_float=True):
         if skip:
             skip = False
         else:
-            for ch
-            raw.append(event['groups'][adc_ch // 2]['data'][adc_ch % 2])
-    print('Raw loaded.')
+            if curr == event_ind:
+                for ch_ind in range(len(adc_chs)):
+                    raw[ch_ind] = event['groups'][adc_chs[ch_ind]['gr']]['data'][adc_chs[ch_ind]['ch']]
+                break
+            else:
+                curr += 1
+    print('Raw loaded.\n')
+
+
+for cell in range(1024):
+    line = ''
+    for ch in range(len(adc_chs)):
+        line += '%.2f, ' % raw[ch][cell]
+    print(line[:-2])
