130403fb34475911fa848e3064fd1cae69667f0e
rezenter <nisovru@gmail.com>
2022-01-24 22:29:43 +0300

    julia serv initial

---
+++ julia/requestHandler.jl
@@ -0,0 +1,57 @@
+using Sockets
+
+module RequestHandler
+    export handle
+
+    function handle(req)
+        @debug req
+        if !haskey(req, "subsystem")
+            return "Subsystem field is missing!"
+        end
+        if !haskey(req, "subsystem")
+            return "Subsystem field is missing!"
+        end
+        if !haskey(table, req.subsystem)
+            return "Handling table has no subsystem " + req.subsystem
+        end
+        if !haskey(req, "reqtype")
+            return "reqtype field is missing!"
+        end
+        if !haskey(table[req.subsystem], req.reqtype)
+            return "Handling table has no reqtype " + req.reqtype
+        end
+        return table[req.subsystem][req.reqtype](req)
+    end
+
+    function calc(req)
+        @debug "call"
+        # async here!, but status must be immediate
+        return "success"
+    end
+
+    function crateConnect(req)
+        @debug "call"
+        resp = Dict{String, Dict}()
+        # async here!, but status must be immediate
+        socket = Sockets::connect("192.168.10.43", 8100)
+        #TcpSocket(open, 0 bytes waiting)
+
+        resp["ok"] = true
+        return resp
+    end
+
+    function diagStatus(req)
+        @debug "call"
+        
+        return "success"
+    end
+
+    table = Dict{String, Dict}()
+    table["process"] = Dict{String, Function}([
+        ("calc", calc)
+    ])
+    table["diag"] = Dict{String, Function}([
+        ("crate_connect", crateConnect),
+        ("status", diagStatus)
+    ])
+end
---
+++ julia/server.jl
@@ -0,0 +1,51 @@
+using HTTP
+using Sockets
+using JSON3
+
+include("requestHandler.jl")
+
+ENV["JULIA_DEBUG"] = "all"
+
+@info "startup"
+@debug "Debug is enabled!"
+@debug "Thread count for Julia = ", Threads.nthreads()
+
+function handler(req::HTTP.Request)
+    if req.method == "GET"
+        if req.target == "/"
+            req.target =  "html/julia_index.html"
+        else
+            req.target = req.target[2:end]
+        end
+        if !isfile(req.target)
+            @debug req.target
+            return HTTP.Response(404, "Whoops, file not found.")
+        end
+        out::String = "Failed to open file"
+        open(req.target) do file
+            out = read(file, String)
+        end
+        return HTTP.Response(200, out)
+    end
+    
+
+    if size(HTTP.payload(req)) == 0
+        @debug req
+        return HTTP.Response(404, "No payload found in request.")
+    else
+        return HTTP.Response(200, JSON3.write(RequestHandler.handle(JSON3.read(HTTP.payload(req)))))
+    end
+    
+end
+
+using HTTP
+
+HTTP.serve(Sockets.localhost, 8081) do request::HTTP.Request
+    try
+        return handler(request)
+    catch e
+        @error "Cought an error in the serve() loop"
+        @debug e
+        return HTTP.Response(404, "Error: $e")
+    end
+end
